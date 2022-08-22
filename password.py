23cabdce0c6765bf7970df172ed02c1fd6920a8b
Muthuram-coder <muthuram.tech@gmail.com>
2022-08-22 04:36:27 -0700

    First commit

---
+++ password.py
@@ -0,0 +1,12 @@
+import string
+import random
+password = list(string.ascii_letters + string.digits + "!@#$%^&*")
+def password_generator():
+    length = int(input("Enter the size of the password you want to create: "))
+    
+    main = []
+    for i in range(length):
+        main.append(random.choice(password))
+    random.shuffle(main)
+    print("".join(main))
+password_generator()
