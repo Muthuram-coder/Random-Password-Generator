2294645b187474d052b68c38a23992dc2176fca1
Muthuram-coder <muthuram.tech@gmail.com>
2022-08-21 23:51:54 -0700

    small change

--- password.py
+++ password.py
@@ -4,7 +4,7 @@ import random
 password = list(string.ascii_letters + string.digits + "!@#$%^&*")
 def password_generator():
     length = int(input("Enter the size of the password you want to create: "))
-    random.shuffle(password)
+    
     main = []
     for i in range(length):
         main.append(random.choice(password))
